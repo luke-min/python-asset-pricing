ea6b57b1eac73bf2f1b01ae619ee99431c0ffbde
Luke Min <luke.kmin@gmail.com>
2024-04-03 23:44:46 -0400

    Deleted Amazon ticker. Does not seem to work

--- PricePullAndStore/pps_settings.py
+++ PricePullAndStore/pps_settings.py
@@ -1,6 +1,5 @@
 # Settings for PricePullAndStore
 
-YF_TICK_LIST = ["AAPL", "AMAZ", "GOOG", "MSFT"]
+YF_TICK_LIST = ["AAPL", "GOOG", "MSFT"]
+# YF_TICK_LIST = ["AAPL", "AMAZ", "GOOG", "MSFT"]
 DEFAULT_START_DT = '1995-01-01'
-
-
